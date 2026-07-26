8ccf20e418a49a2a9dda28117c04df20da5746d3
Assamee <assameeclayton12@gmail.com>
2026-07-26 23:36:56 +0100

    Tested that given test data was rendered correctly on the page

--- testing/App.test.jsx
+++ testing/App.test.jsx
@@ -3,22 +3,52 @@ import { describe, it, expect, beforeEach } from 'vitest';
 import App from '../src/App.jsx'; // No need for { } because the App component is the default export
 
 // Things to test:
-// 1. Render the initial 4 companies
+// 1. Render the initial applications (the test data)
 // 2. Search (typing filters rows + the company/role toggle)
 // 3. Adding a new application
 // 4. Edit an existing application
 // 5. Delete an application (Confirm removes a row/Cancel doesn't)
 // 6. Persistance (localStorage)
 
+const testdata = JSON.stringify(
+    [
+        {
+            id: 1,
+            company: "Barclays",
+            role: "Technology Developer Intern",
+            status: "Offer Received!",
+        },
+        {
+            id: 2,
+            company: "Google",
+            role: "Software Engineer Intern",
+            status: "Interview",
+        },
+        {
+            id: 3,
+            company: "Microsoft",
+            role: "Product Manager Intern",
+            status: "Yet to Apply",
+        }
+    ]
+);
+
 beforeEach(() => {
     localStorage.clear();
+
+    localStorage.setItem('dashboard_applications', testdata);
+
 });
 
-// Test 1: Render the initial 4 companies
+
+
+// Test 1: Render the initial applications
 
 describe("Render the initial applications", () => {
     it("Renders the initial applications", () => {
         render(<App />);
-        expect(screen.getByText("Software Engineering Intern")).toBeInTheDocument();
+        expect(screen.getByText(/Barclays/i)).toBeInTheDocument();
+        expect(screen.getByText(/Google/i)).toBeInTheDocument();
+        expect(screen.getByText(/Microsoft/i)).toBeInTheDocument();
     });
 });
